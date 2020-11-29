2453d4bc0f05deaac4dd0adc72378d2a5578dc5b
zwartapps <zwart.apps@gmail.com>
2020-11-29 13:30:31 +0000

    formCitas updated

--- citas/formCitas.php
+++ citas/formCitas.php
@@ -15,6 +15,11 @@ require_once("../class/class.Cita.php");
 // Cargamos el usuario
 $usuario = new Usuario($GLOBAL_SESSION[CAMPO_DATOS_SESION]['id']);
 
+//Cargamos los datos de las citas
+$cita = new Cita();
+
+$paciente = new Paciente();
+
 // Cargamos los datos del Rol
 $rolUsuario = new Rol($usuario->idRol);
 
@@ -37,63 +42,69 @@ if (!$permisosWeb->permitido) {
 // Comprobamos si se ha cargado el formulario y hay que guardar los datos
 if (isset($_GET['guardar'])) {
     if ($_GET['id'] != 0) {
-        // PAciente ya existente -> Hay que modificar
-        $pacienteGuardar = new Paciente($_GET['id']);
+        // Cita ya existente -> Hay que modificar
+        $citaGuardar = new Cita($_GET['id']);
     } else {
-        $pacienteGuardar = new Paciente();
-        $pacienteGuardar->idRol = 4;
-        $pacienteGuardar->numIntentosLogin = 0;
-        $pacienteGuardar->ultimoAcceso = date("Y-m-d H:i:s");
+        $citaGuardar = new Cita();
+        /*
+        $citaGuardar->idRol = 4;
+        $citaGuardar->numIntentosLogin = 0;
+        $citaGuardar->ultimoAcceso = date("Y-m-d H:i:s");
+        */
     }
 
+    /* 
     foreach($_POST as $clave => $valor) {
         if ($clave != 'password') {
-            $pacienteGuardar->$clave = $valor;
+            $citaGuardar->$clave = $valor;
         } else {
             if ($valor != '') {
                 // Actualizamos la contraseña
-                $pacienteGuardar->setPassword($valor);
+                $citaGuardar->setPassword($valor);
             }
         }
-    }
+    } 
+    */
 
-    if ($pacienteGuardar->guardar()) {
+    if ($citaGuardar->guardar()) {
         $mensajeGuardado = 'SE HA CREADO/ACTUALIZADO EL USUARIO';
     } else {
         $mensajeGuardado = 'HA OCURRIDO UN ERROR AL INTENTAR GUARDAR/CREAR EL USUARIO';
     }
-    $id = $pacienteGuardar->id;
+    $id = $citaGuardar->id;
 }
 
 // Primero comprobamos si se trata de la edición de un usuario existente
 if (isset($_GET['id'])) {
     // Cargamos los datos del usuario existente
     $id = $_GET['id'];
-    $paciente = new Paciente($id);
+    $cita = new Cita($id);
 } else {
     $id = 0;
-    $paciente = new Paciente();
-    $paciente->idRol = 4;
+    $cita = new Cita();
+    //  $paciente->idRol = 4;
 }
 
-if (isset($pacienteGuardar)) {
-    $id = $pacienteGuardar->id;
-    $paciente = $pacienteGuardar;
+if (isset($citaGuardar)) {
+    $id = $citaGuardar->id;
+    $cita = $citaGuardar;
 }
+/*
 
 $rolPaciente = new Rol($paciente->idRol);
 
-// Cargamos los roles
+//Cargamos los roles
 $roles = cargarRoles('FETCH_OBJ');
 $htmlRoles = "";
 foreach($roles as $rol) {
-	if ($rol->id == $paciente->idRol) {
-		$htmlRoles .= "<option value='{$rol->id}' selected>{$rol->nombre}</option>";
-	} else {
-		$htmlRoles .= "<option value='{$rol->id}'>{$rol->nombre}</option>";
-	}
+    if ($rol->id == $paciente->idRol) {
+        $htmlRoles .= "<option value='{$rol->id}' selected>{$rol->nombre}</option>";
+    } else {
+        $htmlRoles .= "<option value='{$rol->id}'>{$rol->nombre}</option>";
+    }
 }
-
+*/
+/*
 // Preparamos el select para ver si está bloqueado o no
 $htmlBloqueado = "";
 if ($paciente->bloqueado) {
@@ -103,34 +114,37 @@ if ($paciente->bloqueado) {
     $htmlBloqueado .= "<option value='1'>SÍ</option>";
     $htmlBloqueado .= "<option selected value='0'>NO</option>";
 }
+*/
+
+//$cita = new Cita($cita->idCita);
 
 // Cargamos los tipos de Cita
 $citas = cargarCitas('FETCH_OBJ');
 $htmlCitas = "";
-foreach($citas as $cita) {
-	if ($cita->id == $paciente->idCupo) {
-		$htmlCupos .= "<option value='{$cita->id}' selected>{$cupo->nombre}</option>";
-	} else {
-		$htmlCupos .= "<option value='{$cita->id}'>{$cupo->nombre}</option>";
-	}
-
+foreach ($citas as $cita) {
+    if ($cita->id == $paciente->idPaciente) {
+        $htmlCitas .= "<option value='{$cita->id}' selected>{$cita->tipo}</option>";
+    } else {
+        $htmlCitas .= "<option value='{$cita->id}'>{$cita->tipo}</option>";
+    }
 }
-
+/*
 // Cargamos los cupos
 $cupos = cargarCupos('FETCH_OBJ');
 $htmlCupos = "";
 foreach($cupos as $cupo) {
-	if ($cupo->id == $paciente->idCupo) {
-		$htmlCupos .= "<option value='{$cupo->id}' selected>{$cupo->nombre}</option>";
-	} else {
-		$htmlCupos .= "<option value='{$cupo->id}'>{$cupo->nombre}</option>";
-	}
+    if ($cupo->id == $paciente->idCupo) {
+        $htmlCupos .= "<option value='{$cupo->id}' selected>{$cupo->nombre}</option>";
+    } else {
+        $htmlCupos .= "<option value='{$cupo->id}'>{$cupo->nombre}</option>";
+    }
 }
-
+*/
 
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <title>Pruebas PHP - CIP</title>
     <meta charset="UTF-8">
@@ -141,111 +155,85 @@ foreach($cupos as $cupo) {
 
 
 <body>
-<!-- Incluimos el menú de navegación -->
-<?php include("../menu/".$rolUsuario->menuWeb); ?>
+    <!-- Incluimos el menú de navegación -->
+    <?php include("../menu/" . $rolUsuario->menuWeb); ?>
 
-<div class="container">
-    <div class="row mt-4">
-        <h3>Crear una Cita</h3>
-        <div class="col-md-12 mt-4">
-            <form action="formPaciente.php?guardar=1&id=<?php echo $id; ?>" method="POST">
-                <!-- Campo oculto con el id a modificar -->
-                <input type="hidden" name="id" value="<?php echo $id; ?>">
+    <div class="container">
+        <div class="row mt-4">
+            <h3>Crear una Cita</h3>
+            <div class="col-md-12 mt-4">
+                <form action="formCitas.php?guardar=1&id=<?php echo $id; ?>" method="POST">
+                    <!-- Campo oculto con el id a modificar -->
+                    <input type="hidden" name="id" value="<?php echo $id; ?>">
 
-                <!-- Campos nombre y apellidos -->
-                <div class="row">
-                    <div class="col-md-3">
-                        <label class="sr-only" for="inlineFormInputGroup">Nombre</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Nombre</div>
+                    <!-- Campos nombre y apellidos -->
+                    <div class="row">
+                        <div class="col-md-3">
+                            <label class="sr-only" for="inlineFormInputGroup">Nombre</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Nombre</div>
+                                </div>
+                                <input name="nombre" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Nombre" value="<?php echo $paciente->nombre; ?>">
                             </div>
-                            <input name="nombre" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Nombre" value="<?php echo $paciente->nombre; ?>">
+                        </div>
+                        <div class="col-md-5">
+                            <label class="sr-only" for="inlineFormInputGroup">Apellidos</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Apellidos</div>
+                                </div>
+                                <input name="apellidos" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Apellidos" value="<?php echo $paciente->apellidos; ?>">
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <label class="sr-only" for="inlineFormInputGroup">Email</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Email</div>
+                                </div>
+                                <input name="email" type="email" class="form-control" id="inlineFormInputGroup" placeholder="Correo electrónico" value="<?php echo $paciente->email; ?>">
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-5">
-                        <label class="sr-only" for="inlineFormInputGroup">Apellidos</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Apellidos</div>
+                    <hr>
+                    <!-- Campos Tipo Cita y Fecha Hora -->
+                    <div class="row">
+                        <div class="col-md-4">
+                            <label class="sr-only" for="inlineFormInputGroup">Tipo Cita</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Tipo Cita</div>
+                                </div>
+                                <select name="tipo">
+                                    <?php echo $htmlCitas; ?>
+                                </select>
                             </div>
-                            <input name="apellidos" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Apellidos" value="<?php echo $paciente->apellidos; ?>">
+                        </div>
+                        <div class="col-md-4">
+                            <label class="sr-only" for="inlineFormInputGroup">Fecha y Hora</label>
+                            <div class="input-group mb-2">
+                                <div class="input-group-prepend">
+                                    <div class="input-group-text">Fecha y Hora</div>
+                                </div>
+                                <input name="fechaHora" type="datetime-local" class="form-control" id="inlineFormInputGroup" placeholder="Fecha y Hora" value="<?php echo $citas->fechaHora; ?>">
+                            </div>
                         </div>
                     </div>
-                    <div class="col-md-5">
-                        <label class="sr-only" for="inlineFormInputGroup">Email</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Email</div>
-                            </div>
-                            <input name="email" type="email" class="form-control" id="inlineFormInputGroup" placeholder="Correo electrónico" value="<?php echo $paciente->email; ?>">
-                        </div>
+                    <div class="text-right">
+                        <button type="button" class="btn btn-danger" onclick="window.history.back();">Cancelar</button>
+                        <button type="submit" class="btn btn-success">Guardar</button>
                     </div>
-                </div>         
-                
-                <hr>
-				
-				<!-- Campos centro de salud y cupo -->
-                <div class="row">
-                    <div class="col-md-4">
-                        <label class="sr-only" for="inlineFormInputGroup">Tipo Cita</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Tipo Cita</div>
-                            </div>
-                            <select name="tipo">
-								<?php echo $htmlCitas; ?>
-							</select>
-                        </div>
-                    </div>
-                    <div class="col-md-2">
-                        <label class="sr-only" for="inlineFormInputGroup">Cupo</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Cupo</div>
-                            </div>
-                            <select name="idCupo">
-								<?php echo $htmlCupos; ?>
-							</select>
-                        </div>
-                    </div>
-                    <div class="col-md-3">
-                        <label class="sr-only" for="inlineFormInputGroup">Nº Historial</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Nº Historial</div>
-                            </div>
-                            <input name="numHistoria" type="text" class="form-control" id="inlineFormInputGroup" placeholder="Nº Historial" value="<?php echo $paciente->numHistoria; ?>">
-                        </div>
-                    </div>
-
-                </div>
-                    <div class="col-md-4">
-                        <label class="sr-only" for="inlineFormInputGroup">Fecha y Hora</label>
-                        <div class="input-group mb-2">
-                            <div class="input-group-prepend">
-                                <div class="input-group-text">Fecha y Hora</div>
-                            </div>
-                            <input name="fechaHora" type="datetime-local" class="form-control" id="inlineFormInputGroup" placeholder="Fecha y Hora" value="<?php echo $citas->fechaHora; ?>">
-                        </div>
-                    </div>
-
-   
-                </div>				
-				<div class="text-right">
-					<button type="button" class="btn btn-danger" onclick="window.history.back();">Cancelar</button>
-					<button type="submit" class="btn btn-success">Guardar</button>
-				</div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
 
 
-<footer class="footer">
-    <!-- Incluimos el menú de navegación -->
-    <?php include("../footer.php"); ?>
-</footer>
+    <footer class="footer">
+        <!-- Incluimos el menú de navegación -->
+        <?php include("../footer.php"); ?>
+    </footer>
 </body>
 
 </html>
--- class/class.Cita.php
+++ class/class.Cita.php
@@ -7,7 +7,7 @@ class Cita {
     protected $fechaHora;
     protected $idPaciente;       
     protected $tipo;
-
+    
     public function __construct($id = 0) {
         if ($id != 0) {
             // Consultamos los datos por id en la BD
@@ -21,7 +21,7 @@ class Cita {
         }
         return true;
     }
-
+    
     public function __set($atributo, $valor) {
         if (property_exists($this,$atributo)) {
             $this->$atributo = $valor;
@@ -29,23 +29,105 @@ class Cita {
         }
         return false;
     }
-
+    
     public function __get($atributo) {
         if (property_exists($this,$atributo)) {
             return $this->$atributo;
         }
         return false;
     }
+    
+    
+    public function guardar() {
+        $gestorDB = new GestorDB();
+        
+        if ($this->id != 0) {
+            // Hay que hacer un UPDATE
+            // Variables para la tabla USUARIOS
+            $datosCita = array(
+                'id' => $this->id,
+                'tipo' => $this->tipo,
+                'fechaHora' => $this->fechaHora,
+                'idPaciente' => $this->idPaciente                
+            );
+            
+            $datosPaciente = array(
+                'id' => $this->id,
+                'idCentroSalud' => $this->idCentroSalud,
+                'idCupo' => $this->idCupo,
+                'numHistoria' => $this->numHistoria
+            );
+            $clavesPrimarias = array('id' => $this->id);
+            
+            // Actualizamos la tabla de Citas
+            $resultadoCita = $gestorDB->updateDB(TABLA_CITAS, $datosCita, $clavesPrimarias);
+            if ($resultadoCita instanceof PDOException) {
+                // Ha ocurrido un error
+                // Hay que insertar en el log
+                return false;
+            }
+            
+            /* Actualizamos la tabla de Pacientes
+            $resultadoUsuario = $gestorDB->updateDB(TABLA_PACIENTES, $datosPaciente, $clavesPrimarias);
+            if ($resultadoUsuario instanceof PDOException) {
+                // Ha ocurrido un error
+                // Hay que insertar en el log
+                return false;
+            }*/
+            
+            return true;
+        } else {
+            // Hay que hacer un INSERT
+            $datosCita = array(
+                'id' => $this->id,
+                'tipo' => $this->tipo,
+                'fechaHora' => $this->fechaHora,
+                'idPaciente' => $this->idPaciente       
+            );
+            
+            /* Insertamos al usuario en la tabla de Usuarios
+            $resultadoUsuario = $gestorDB->insertIntoDB(TABLA_USUARIOS, $datosUsuario, ['id']);
+            if ($resultadoUsuario instanceof PDOException) {
+                // Ha ocurrido un error
+                // Hay que insertar en el log
+                echo $resultadoUsuario->getMessage();
+                return false;
+            } else {
+                $this->id = $resultadoUsuario;
+            }
+           
+            $datosPaciente = array(
+                'id' => $this->id,
+                'numHistoria' => $this->numHistoria,
+                'idCentroSalud' => $this->idCentroSalud,
+                'idCupo' => $this->idCupo
+            );
+            
+            // Insertamos al médico en la tabla de pacientes
+            $datosPaciente = $gestorDB->insertIntoDB(TABLA_PACIENTES, $datosPaciente, []);
+            if ($datosPaciente instanceof PDOException) {
+                // Ha ocurrido un error
+                // Hay que insertar en el log
+                echo $datosPaciente->getMessage();
+                return false;
+            }*/
+            
+            return true;
+        }
+    }
+    
+     
+    
+   
+    
 }
 
-// Devuelve un array en PHP con los datos de todos las citas
-function cargarCitas($tipoFetch = 'FETCH_ASSOC',$idPaciente = 0) {
-	$gestorDB = new GestorDB();
-	if ($idPaciente != 0) {
-        $registros = $gestorDB->getRecordsByParams(TABLA_CITAS, ['*'], 'idPaciente = '.$idPaciente, NULL, $tipoFetch);
-        return $registros;
-	} 	
-	
+ // Devuelve un array en PHP con los datos de todos las citas
+ function cargarCitas ($tipoFetch = 'FETCH_ASSOC') {
+    $gestorDB = new GestorDB();
+    $registros = $gestorDB->getRecordsByParams(TABLA_CITAS, ['*'], NULL, NULL, $tipoFetch);
+
+    return $registros;       
 }
 
 ?>
